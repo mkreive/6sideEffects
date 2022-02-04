37d69b68eb9c5868feb618d3035695ff4e3d6567
mkreive <mkreive@gmail.com>
2022-02-04 15:08:18 +0200

    useEffect Cleanup function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,16 @@ const Login = (props) => {
     const [formIsValid, setFormIsValid] = useState(false);
 
     useEffect(() => {
-        setFormIsValid(
-            enteredEmail.includes("@") && enteredPassword.trim().length > 6
-        );
+        const identifier = setTimeout(() => {
+            setFormIsValid(
+                enteredEmail.includes("@") && enteredPassword.trim().length > 6
+            );
+        }, 500);
+
+        //Cleanup function - to clean all timouts
+        return () => {
+            clearTimeout(identifier);
+        };
     }, [enteredEmail, enteredPassword]);
 
     const emailChangeHandler = (event) => {
